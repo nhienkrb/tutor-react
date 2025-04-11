16d4f50a4f23adf79012cc7d02fd5d242c9df078
nhiendev <nhiennkrb@gmail.com>
2025-04-12 02:00:30 +0700

    knowledge: condition rendering list items

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,10 +1,21 @@
+import Fruit from "./Fruit";
+
 function displayMessage() {
   return "Hello, this is a message from the displayMessage function!";
 }
-
+const foodList = [
+  {name:"banana", price: 1.2, color: "yellow"},
+  {name:"apple", price: 0.8, color: "red"},
+  {name:"orange", price: 1.5, color: "orange"},
+  {name:"grape", price: 2.0, color: "purple"},
+  {name:"kiwi", price: 1.8, color: "green"},
+]
 export default function Foods() {
-  const food = "Banana";
   return (
-    <div>This is a components list Foods: food one: {food}  - food two: {displayMessage()}</div>
+    <div>
+      {foodList.map((food, index) => (
+        <Fruit key={index} fruit={food} />
+      ))}
+    </div>
   );
 }
