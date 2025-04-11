b05c04ac9580ccd46361dee85c43ae506f506ff4
nhiendev <nhiennkrb@gmail.com>
2025-04-11 22:47:24 +0700

    feat: JSX in react

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,7 +1,10 @@
+function displayMessage() {
+  return "Hello, this is a message from the displayMessage function!";
+}
+
 export default function Foods() {
-
-    return (
-        <div className="">This is a components list Foods</div>
-    )
-
+  const food = "Banana";
+  return (
+    <div>This is a components list Foods: food one: {food}  - food two: {displayMessage()}</div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,5 @@ import Foods from './components/Foods'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Foods />
   </React.StrictMode>,
 )
