d92639de2dd1bcf60b46f9c141f03fdeb9763f24
nhiendev <nhiennkrb@gmail.com>
2025-04-12 23:12:38 +0700

    #6 knowledge: Create Multiple States In React

--- src/components/demo-state/couter.jsx
+++ src/components/demo-state/couter.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  return <div>Count Value is: {count}
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
-  </div>;
+  const [increaseBy, setIncreaseBy] = useState(1);
+  return (
+    <div>
+      Count Value is: {count}
+      <button onClick={() => setCount(increaseBy + count)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    <h3>Increase By: {increaseBy}</h3>
+      <button onClick={() => setIncreaseBy(increaseBy + 1)}>Increase by</button>
+    </div>
+  );
 }
